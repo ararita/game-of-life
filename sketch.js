3cdcf93b05ed050fe5135ea506ddef3460480fb6
ararita <munjizavesna@gmail.com>
2021-04-07 16:33:38 +0200

    Render grid

--- sketch.js
+++ sketch.js
@@ -9,14 +9,34 @@ function make2DArray(cols, rows) {
   return arr;
 }
 
-let cols = 10;
-let rows = 10;
+let cols;
+let rows;
+let resolution = 20;
 
 function setup() {
+  createCanvas(800, 800);
+  cols = width / resolution;
+  rows = height / resolution;
+
   grid = make2DArray(cols, rows);
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
       grid[i][j] = floor(random(2));
+    }
+  }
+}
+
+function draw() {
+  background(0);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let x = i * resolution;
+      let y = j * resolution;
+      if (grid[i][j] === 1) {
+        fill(255);
+        // stroke(255);
+        rect(x, y, resolution, resolution);
+      }
     }
   }
 }
